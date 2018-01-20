eb0b6ccddcf9efa054fed4491d2adbb646d589b1
Oliver Mensah <olivermensah96@gmail.com>
2018-01-20 22:02:31 +0000

    usage outline

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -24,7 +24,6 @@
     </div>
     <div class="content-description">
     <p   align="center">A microframework for building web application with PHP.  To use this framework kindly <a href="https://github.com/OMENSAH/barePHP/blob/master/README.md#how-to-use--barephp">use the docs</a></p>
-
     </div>
 
     
