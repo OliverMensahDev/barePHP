f1912047d1c1cec18b5c70cc90c0a38fb1e45640
Oliver Mensah <olivermensah96@gmail.com>
2018-02-03 19:26:00 +0000

    added

--- application/core/Database.php
+++ application/core/Database.php
@@ -6,84 +6,81 @@
  * Bind Values 
  * Return rows and results 
  */
-class Database{
-    private $db_host = 'localhost';
-    private $db_user = 'root';
-    private $db_pass ='';
-    private $db_name ='goodquotes';
-    protected $dbh;
-    protected $stmt;
-
-    public function __construct(){
-      try{
-        $this->dbh = new PDO("mysql:host=".$this->db_host.";dbname=".$this->db_name,$this->db_user, $this->db_pass);
-      } catch(PDOException $e){
-        echo '<div class="alert alert-danger">'.get_class($e).' on line '.$e->getLine().' of '.$e->getFile().': '.$e->getMessage().'</div>';
-      }
-    }
-    //Prepared Statement
-    public function query($query){
-      try{
-        $this->stmt = $this->dbh->prepare($query);
-      } catch(Throwable $e){
-        echo '<div class="alert alert-danger">'.get_class($e).' on line '.$e->getLine().' of '.$e->getFile().': '.$e->getMessage().'</div>';
-      }
-    }
-    //Bind Values
-    public function bind($param, $value, $type = null){
-        try {
-            if(is_null($type)){
-              switch(true){
-                case is_int($value):
-                  $type = PDO::PARAM_INT;
-                  break;
-                case is_bool($value):
-                  $type = PDO::PARAM_BOOL;
-                  break;
-                case is_null($value):
-                  $type = PDO::PARAM_NULL;
-                  break;
-                default:
-                  $type = PDO::PARAM_STR;
-              }
-            }
-            $this->stmt->bindValue($param, $value, $type);
-        } catch (Throwable $e) {
-            echo '<div class="alert alert-danger">'.get_class($e).' on line '.$e->getLine().' of '.$e->getFile().': '.$e->getMessage().'</div>';
-        }
-    }
-    //Execute Prepared Statement
-    public function execute(){
-      try{
-        $this->stmt->execute();
-      }catch(Throwable $e){
-        echo '<div class="alert alert-danger">'.get_class($e).' on line '.$e->getLine().' of '.$e->getFile().': '.$e->getMessage().'</div>';
-      }
-    }
-    //Get All Results
-    public function resultSet():array{
-      try{
-        $this->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-      }catch(Throwable $e){
-        echo '<div class="alert alert-danger">'.get_class($e).' on line '.$e->getLine().' of '.$e->getFile().': '.$e->getMessage().'</div>';
-      }
-    }
-    //Get Last Inserted Item's Id
-    public function lastInsertId(){
-      return $this->dbh->lastInsertId();
-    }
-    //Get a Single Item
-    public function single(){
-      try{
-        $this->execute();
-        return $this->stmt->fetch(PDO::FETCH_ASSOC);
-      } catch(Throwable $e){
-        echo '<div class="alert alert-danger">'.get_class($e).' on line '.$e->getLine().' of '.$e->getFile().': '.$e->getMessage().'</div>';
-      }
-    }
-    //Get Affected Row Count for a query 
-    public function rowCount(){
-        return $this->stmt->rowCount();
-    }
-}
+ class Database {
+ 	private $host = DB_HOST;
+ 	private $user = DB_USER;
+ 	private $pass = DB_PASS;
+ 	private $dbname = DB_NAME;
+ 	
+ 	private $dbh;
+ 	private $error;
+ 	private $stmt;
+ 	
+ 	public function __construct() {
+ 		// Set DSN
+ 		$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+ 		$options = array (
+ 			PDO::ATTR_PERSISTENT => true,
+ 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION 
+ 		);
+ 		// Create a new PDO instanace
+ 		try {
+ 			$this->dbh = new PDO ($dsn, $this->user, $this->pass, $options);
+ 		}		// Catch any errors
+ 		catch ( PDOException $e ) {
+ 			$this->error = $e->getMessage();
+ 		}
+ 	}
+ 	
+ 	// Prepare statement with query
+ 	public function query($query) {
+ 		$this->stmt = $this->dbh->prepare($query);
+ 	}
+ 	
+ 	// Bind values
+ 	public function bind($param, $value, $type = null) {
+ 		if (is_null ($type)) {
+ 			switch (true) {
+ 				case is_int ($value) :
+ 					$type = PDO::PARAM_INT;
+ 					break;
+ 				case is_bool ($value) :
+ 					$type = PDO::PARAM_BOOL;
+ 					break;
+ 				case is_null ($value) :
+ 					$type = PDO::PARAM_NULL;
+ 					break;
+ 				default :
+ 					$type = PDO::PARAM_STR;
+ 			}
+ 		}
+ 		$this->stmt->bindValue($param, $value, $type);
+ 	}
+ 	
+ 	// Execute the prepared statement
+ 	public function execute(){
+ 		return $this->stmt->execute();
+ 	}
+ 	
+ 	// Get result set as array of objects
+ 	public function resultset(){
+ 		$this->execute();
+ 		return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+ 	}
+ 	
+ 	// Get single record as object
+ 	public function single(){
+ 		$this->execute();
+ 		return $this->stmt->fetch(PDO::FETCH_OBJ);
+ 	}
+ 	
+ 	// Get record row count
+ 	public function rowCount(){
+ 		return $this->stmt->rowCount();
+ 	}
+ 	
+ 	// Returns the last inserted ID
+ 	public function lastInsertId(){
+ 		return $this->dbh->lastInsertId();
+ 	}
+ }
